e5032691286c0a6c5afef37ded8360a835fe7505
esmar976 <asmae.ramzi@uit.ac.ma>
2023-10-16 12:51:50 -0700

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *_strlen-Returns the length of a string
- * 
- *Return:length
- */
+*_strlen-Returns the length of a string
+*@s:string
+*Return:length
+*/
 
 int _strlen(char *s)
 {

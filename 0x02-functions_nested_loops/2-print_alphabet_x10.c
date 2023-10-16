e760ea1fd05807557fbd7f18bea30715346419fb
esmar976 <asmae.ramzi@uit.ac.ma>
2023-10-16 05:24:23 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet 
- * 
- * Return:void
+ *print_alphabet_x10-prints 10 times the alphabet.
+ *Return:void
  */
 
 void print_alphabet_x10(void)
